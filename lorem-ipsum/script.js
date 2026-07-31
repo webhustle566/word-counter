8b3894a378b15b78ddd9942865003c01e0963c28
webhustle566 <310778624+webhustle566@users.noreply.github.com>
2026-07-31 17:28:19 -0400

    Added Lorem Ipsum Generator and updated SEO

---
+++ lorem-ipsum/script.js
@@ -0,0 +1,76 @@
+const paragraphs = [
+
+`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+
+`Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.`,
+
+`Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.`,
+
+`Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.`,
+
+`Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam.`,
+
+`Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum.`,
+
+`Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla.`,
+
+`Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod.`,
+
+`Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.`,
+
+`Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum.`
+
+];
+
+
+
+function generateLorem() {
+
+    let amount = document.getElementById("amount").value;
+
+    let output = "";
+
+    let usedParagraphs = [];
+
+
+    while (usedParagraphs.length < amount) {
+
+        let random = Math.floor(Math.random() * paragraphs.length);
+
+
+        if (!usedParagraphs.includes(random)) {
+
+            usedParagraphs.push(random);
+
+            output += paragraphs[random] + "\n\n";
+
+        }
+
+    }
+
+
+    document.getElementById("output").value = output;
+
+}
+
+
+
+function copyText() {
+
+    let text = document.getElementById("output");
+
+    text.select();
+
+    navigator.clipboard.writeText(text.value);
+
+    alert("Copied!");
+
+}
+
+
+
+function clearText() {
+
+    document.getElementById("output").value = "";
+
+}
